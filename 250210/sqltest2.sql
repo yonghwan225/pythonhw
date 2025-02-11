e9ae3ce6688474dc7c96d2adfa9e615ddebca58f
kopo11 <kbob48624@gmail.com>
2025-02-11 14:19:40 +0900

    Update sqltest2.sql

--- 250210/sqltest2.sql
+++ 250210/sqltest2.sql
@@ -1,23 +1,4 @@
 create database temp;
-create table students(id int(5) auto_increment primary key,
-name char(5),
-age int(5)
-);
-
-insert into students(name, age) values('kim', 25);
-insert into students(name, age) values('lee', 37);
-insert into students(name, age) values('park', 26);
-insert into students(name, age) values('choi', 35);
-insert into students(name, age) values('jung', 34);
-
-select count(*) from students;
-
-select Max(age),min(age) from students;
-*/
-
-
-//
-create database temp;
 create table students(id int(5) auto_increment primary key, name char(10),age int(5),grade char(5));
 
 insert into students values(1, 'Alice', 20, 'A');
