d04e6672f1c8df64b3bc8ddf8079521fd25a6720
kopo11 <kbob48624@gmail.com>
2025-02-11 16:13:08 +0900

    Update c언어1.c

--- 250210/c언어1.c
+++ 250210/c언어1.c
@@ -1,22 +1,21 @@
-
-
 #include <stdio.h>
 
 void main()
 {
-    int a, b, c, result;
+    float a, b, c; 
+    float result;
     printf("숫자 사이에 공백을 넣어 세개 입력 : ");
     while(1)
     {
-        scanf("%d %d %d", &a, &b, &c);
+        scanf("%f %f %f", &a, &b, &c);
         if(c != 0)
         {
             break;
-        }else{
+        }else {
             printf("0으로 나눌수 없습니다");
         }
     }
     result = (a + b) * c - (a - b) / c;
-    printf("연산 결과 : (%d + %d) * %d - (%d - %d) / %d = %d", a, b, c, a, b, c, result);
+    printf("연산 결과 : (%.0f + %.0f) * %.0f - (%.0f - %.0f) / %.0f = %.5f", a, b, c, a, b, c, result);
 
 }
