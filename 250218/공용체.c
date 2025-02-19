e370274bd7b39498165807abfd3164bee223447f
kopo11 <kbob48624@gmail.com>
2025-02-19 09:40:42 +0900

    Update 공용체.c

--- 250218/공용체.c
+++ 250218/공용체.c
@@ -10,16 +10,16 @@ void main()
     };
     
     union a u;
-    scanf("%d %f %c", &u.intVal, &u.floatVal, &u.charVal);
+    scanf("%c %d %f", &u.charVal, &u.intVal, &u.floatVal);
     // scanf("%d", &u.intVal);
     
     // scanf("%f", &u.floatVal);
     
     // scanf("%s", u.charVal);
     
-    
+    printf("%c %d\n", u.charVal, &u.charVal);
     printf("%d %d\n", u.intVal, &u.intVal);
     printf("%f %d\n", u.floatVal, &u.floatVal);
-    printf("%c %d\n", u.charVal, &u.charVal);
+    
 
 }
